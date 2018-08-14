8d86a513cfcb7cc366692e9685adf54a0a1e82ab
Patrick DeVivo <patrick.devivo@gmail.com>
2018-08-14 11:22:09 -0400

    add parsed json response

--- packetmetadata/packetmetadata.go
+++ packetmetadata/packetmetadata.go
@@ -3,19 +3,35 @@ package packetmetadata
 import (
 	"context"
 	"crypto/x509"
+	"encoding/json"
 
 	jsonpatch "github.com/evanphx/json-patch"
 	"github.com/packethost/packetmetadata/hegel"
+	"github.com/packethost/packngo"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 )
 
 var hegelAddr = "metadata.packet.net:50060"
 
+// Metadata is a struct the json response can be serialized into
+type Metadata struct {
+	ID                                 string                   `json:"id"`
+	State                              string                   `json:"state"`
+	Instance                           *packngo.Device          `json:"instance"`
+	PreinstalledOperatingSystemVersion interface{}              `json:"preinstalled_operating_system_version"`
+	NetworkPorts                       []map[string]interface{} `json:"network_ports"`
+	PlanSlug                           string                   `json:"plan_slug"`
+	Facility                           string                   `json:"facility_code"`
+	Hostname                           string                   `json:"hostname"`
+	BondingMode                        int                      `json:"bonding_mode"`
+}
+
 // WatchResult represents a change in metadata
 type WatchResult struct {
-	JSON  []byte
-	Patch []byte
+	JSON     []byte
+	Patch    []byte
+	Metadata *Metadata
 }
 
 // WatchIterator is a struct for iterating over watch results
@@ -38,17 +54,23 @@ func getHegelClient() (hegel.HegelClient, func() error, error) {
 }
 
 // Get returns instance metadata from hegel
-func Get() ([]byte, error) {
+func Get() ([]byte, *Metadata, error) {
 	hegelClient, close, err := getHegelClient()
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 	defer close()
 	res, err := hegelClient.Get(context.Background(), &hegel.GetRequest{})
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
-	return []byte(res.JSON), nil
+
+	var metadata *Metadata
+	err = json.Unmarshal([]byte(res.JSON), &metadata)
+	if err != nil {
+		return nil, nil, err
+	}
+	return []byte(res.JSON), metadata, nil
 }
 
 // Watch returns an iterator of change events
@@ -68,9 +90,16 @@ func Watch() (*WatchIterator, error) {
 		return nil, err
 	}
 	currentState := []byte(res.JSON)
+
+	var metadata *Metadata
+	err = json.Unmarshal(currentState, &metadata)
+	if err != nil {
+		return nil, err
+	}
 	watchResults <- &WatchResult{
-		JSON:  currentState,
-		Patch: nil,
+		JSON:     currentState,
+		Patch:    nil,
+		Metadata: metadata,
 	}
 
 	client, err := hegelClient.Subscribe(ctx, &hegel.SubscribeRequest{})
@@ -94,9 +123,16 @@ func Watch() (*WatchIterator, error) {
 				if err != nil {
 					errorChan <- err
 				}
+
+				var metadata *Metadata
+				err = json.Unmarshal(currentState, &metadata)
+				if err != nil {
+					errorChan <- err
+				}
 				watchResults <- &WatchResult{
-					JSON:  []byte(newResponse.JSON),
-					Patch: patch,
+					JSON:     []byte(newResponse.JSON),
+					Patch:    patch,
+					Metadata: metadata,
 				}
 				currentState = newState
 			}
